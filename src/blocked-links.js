037d96427a7323e77bafaf1b50235f19a962474f
David <davidhvse@gmail.com>
2026-09-23 17:20:15 +0200

    Add blocked domains and links

--- src/blocked-links.js
+++ src/blocked-links.js
@@ -58,6 +58,8 @@ const BLOCKED_DOMAIN_OCR_PATTERNS = BLOCKED_DOMAINS.map((domain) => ({
 }));
 
 const BLOCKED_LINK_PATTERNS = Object.freeze([
+  /(?<![a-z0-9.-])https?:\/\/wa\.me\/13475918870(?:[/?#][^\s<>]*)?(?![a-z0-9.-])/iu,
+  /(?<![a-z0-9.-])https?:\/\/bit\.ly\/3N6b3J3(?:[/?#][^\s<>]*)?(?![a-z0-9.-])/iu,
   /(?<![a-z0-9.-])https?:\/\/surveybuilder\.io\/c\/capture\/mhnkqthus3a(?:[/?#][^\s<>]*)?(?![a-z0-9.-])/iu,
   /(?<![a-z0-9.-])https?:\/\/(?:www\.)?upwork\.com\/freelancers\/~011c10fca307bf0e02(?:[/?#][^\s<>]*)?(?![a-z0-9.-])/iu,
   /(?<![a-z0-9.-])https?:\/\/(?:www\.)?agentrouter\.org\/register\?aff=qaiK(?:[&#][^\s<>]*)?(?![a-z0-9.-])/iu,
